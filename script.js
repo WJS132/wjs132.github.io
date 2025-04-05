d61d38cc0bdef25e171dbdd590d3f028f30db343
WJS132 <johnypl326@gmail.com>
2025-04-05 10:46:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ dropdownKategoria.forEach(el => {
         let linki = this.parentElement.querySelectorAll("a");
         linki.forEach(element => {
             element.style.backgroundColor = `rgba(${rgb}, ${rgb}, ${rgb}, 0.8)`;
-            rgb += 2;
+            rgb += 3;
         });
     });
 });
 
 let img = document.querySelector(".img");
 let spr = 0;
+let telImg = document.querySelector(".tel-img");
 img.addEventListener("click", () => {
     if (spr == 0){
         img.style.backgroundImage = "url('Wjs.png')";
@@ -22,6 +23,29 @@ img.addEventListener("click", () => {
         img.style.backgroundImage = "url('Logo.png')";
         img.style.margin = "0";
         spr--;
-    }
-    
+    }   
+});
+telImg.addEventListener("click", () => {
+    if (spr == 0){
+        telImg.style.backgroundImage = "url('Wjs.png')";
+        spr++;
+    }else{
+        telImg.style.backgroundImage = "url('Logo.png')";
+        spr--;
+    }   
+});
+
+
+let telButton = document.querySelector(".tel-button");
+let overlay = document.querySelector(".overlay");
+let telMenu = document.querySelector(".tel-menu")
+
+telButton.addEventListener("click", function(){
+    telMenu.classList.toggle("active");
+    overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", function(){
+    telMenu.classList.toggle("active");
+    overlay.classList.toggle("active");
 });
